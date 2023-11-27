c4e1b0c4845255b8b18323e0b66ef8444612b0b6
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2023-11-27 12:40:49 -0300

    Update database connection settings in product_routes.py

--- stockpulse/routes/v1/product_routes.py
+++ stockpulse/routes/v1/product_routes.py
@@ -1,41 +1,44 @@
 from fastapi import APIRouter
-from sqlalchemy import Engine, create_engine, text
+from sqlalchemy import Engine, create_engine
 from sqlmodel import SQLModel
 
 from stockpulse.core.controllers.products_controller import ProductsController
 from stockpulse.core.models.product_model import ProductModel
 
+import os
 
-database_url = "mysql+pymysql://root:1234@localhost:3306"
+DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
+DATABASE_PORT = os.getenv('DATABASE_PORT', '3306')
+DATABASE_NAME = os.getenv('DATABASE_NAME', 'stockpulse')
+DATABASE_USER = os.getenv('DATABASE_USER', 'root')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', '123456')
+
+database_url = (f"mysql+mysqlconnector://{DATABASE_USER}:{DATABASE_PASSWORD}"
+                f"@{DATABASE_HOST}:{DATABASE_PORT}")
+
+print("database_url", database_url)
+print("DATABASE_HOST", DATABASE_HOST)
+print("DATABASE_PORT", DATABASE_PORT)
+print("DATABASE_NAME", DATABASE_NAME)
+print("DATABASE_USER", DATABASE_USER)
+print("DATABASE_PASSWORD", DATABASE_PASSWORD)
 
 engine: Engine
 productsController: ProductsController
 
-
 try:
     engine = create_engine(
-        f'{database_url}/stockpulse',
+        f'{database_url}/{DATABASE_NAME}',
         echo=True,
     )
 
-    engine.dispose()
     engine.connect()
     SQLModel.metadata.create_all(engine, checkfirst=True)
     productsController = ProductsController(engine)
+except Exception:
+    print("Error connecting to database")
+    exit()
 
-except Exception as e:
-    print(e)
-    with create_engine(database_url, echo=True).connect() as conn:
-        conn.execute(text("CREATE DATABASE stockpulse"))
-
-    engine = create_engine(
-        f'{database_url}/stockpulse',
-        echo=True,
-    )
-    engine.dispose()
-    engine.connect()
-    SQLModel.metadata.create_all(engine, checkfirst=True)
-    productsController = ProductsController(engine)
 
 products_router = APIRouter(prefix='/products')
 
@@ -49,7 +52,6 @@ def find_all_products():
 @products_router.get('/{product_id}/')
 def find_product_by_uid(product_id: int):
     product = productsController.find_by_id(product_id)
-
     if product is None:
         return {"error": "Product not found"}
     return product
@@ -64,4 +66,8 @@ def create_product(product: ProductModel):
 @products_router.delete('/{product_id}/')
 def delete_product(product_id: int):
     result = productsController.delete(product_id)
-    return {"success": result}
+
+    if result:
+        return {"message": "Product deleted successfully"}
+    else:
+        return {"error": "Product not found"}
