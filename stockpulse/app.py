b88b734a3db88cacc8fb7f38294b6573652df7b8
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2023-12-09 01:26:57 -0300

    Add title and description in fastAPI app

--- stockpulse/app.py
+++ stockpulse/app.py
@@ -3,7 +3,10 @@ from stockpulse.routes.v1.product_routes import products_router
 from fastapi import FastAPI
 
 
-app = FastAPI(debug=True)
+app = FastAPI(
+    title="Stock Pulse API",
+    description="This is a very fancy project,"
+)
 
 app.include_router(router_root)
 app.include_router(products_router, prefix='/v1', tags=['v1'])
