7d01c119e7679502a3f109562b33a3c78eb8701a
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2024-01-30 01:53:16 -0300

    Update database connection settings

--- stockpulse/db_connection.py
+++ stockpulse/db_connection.py
@@ -4,18 +4,18 @@ from sqlmodel import SQLModel
 from stockpulse.core.controllers.product_controller import ProductsController
 import os
 
-DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
-DATABASE_PORT = os.getenv('DATABASE_PORT', '3301')
+DATABASE_URL = os.getenv('DATABASE_URL', 'localhost:3301')
 DATABASE_NAME = os.getenv('DATABASE_NAME', 'stockpulse')
-DATABASE_USER = os.getenv('DATABASE_USER', 'root')
+DATABASE_USER = os.getenv('DATABASE_USER', 'admin')
 DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', '123456')
 
 database_url = (f"mysql+mysqlconnector://{DATABASE_USER}:{DATABASE_PASSWORD}"
-                f"@{DATABASE_HOST}:{DATABASE_PORT}")
+                f"@{DATABASE_URL}")
 
 print("database_url", database_url)
-print("DATABASE_HOST", DATABASE_HOST)
-print("DATABASE_PORT", DATABASE_PORT)
+
+print("database_url", database_url)
+print("DATABASE_HOST", DATABASE_URL)
 print("DATABASE_NAME", DATABASE_NAME)
 print("DATABASE_USER", DATABASE_USER)
 print("DATABASE_PASSWORD", DATABASE_PASSWORD)
